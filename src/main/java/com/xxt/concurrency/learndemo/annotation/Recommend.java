6fc22987b12f01db79cc29d9e9f5bdd9bedb1275
XXT <282973336@qq.com>
2019-04-24 10:32:39 +0800

    annotation

---
+++ src/main/java/com/xxt/concurrency/learndemo/annotation/Recommend.java
@@ -0,0 +1,18 @@
+package com.xxt.concurrency.learndemo.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author xuxiaotong
+ * @date 2019/4/24
+ * @description 用来标记【推荐】的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+
+    String value() default "";
+}
